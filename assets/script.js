bf997db0b1444a8cd71c9720afa3134e667328b1
HHimanshu25 <himanshhukumawat2031@gmail.com>
2025-11-19 22:16:36 +0530

    update

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,64 @@
 let i;
-let profit = 0;
-let due = 0;
-for (i = 1; i <= 4; i++) {
-    let num = document.querySelector(`.cust${i}`);
-    if (num) {
-        // num = num.children[2];  
-        let check = Number(num.children[2].innerHTML)
-        if (check < 0) {
-            num.children[2].style.color = 'red';
-            due = due + check;
+setInterval(() => {
 
+    let profit = 0;
+    let due = 0;
+    for (i = 1; i <= 5; i++) {
+        let num = document.querySelector(`.cust${i}`);
+        if (num) {
+            // num = num.children[2];  
+            let check = Number(num.children[2].innerHTML)
+            if (check < 0) {
+                num.children[2].style.color = 'red';
+                due = due + check;
+
+            }
+            else {
+                num.children[2].style.color = 'rgb(3, 210, 9)'
+                profit = profit + check;
+            }
+            document.querySelector('.num-adv').innerHTML = profit;
+            document.querySelector(".num-due").innerHTML = due;
         }
-        else {
-            num.children[2].style.color = 'rgb(3, 210, 9)'
-            profit = profit + check;
+    }
+}, 50)
+
+
+function nae(v) {
+    let th = document.querySelector(`.cust${v}`).children[1].innerHTML
+    localStorage.setItem("sharenmae", th);
+    localStorage.setItem('nth', v);
+
+}
+
+function update() {
+    let v = localStorage.getItem('nth');
+    if (v) {
+        const money = localStorage.getItem('owntotal');
+        if (money) {
+            document.querySelector(`.cust${v}`).children[2].innerHTML = money;
         }
-        document.querySelector('.num-adv').innerHTML = profit;
-        document.querySelector(".num-due").innerHTML = due;
     }
 }
+update();
 
-
-function nae(v){
-    let th = document.querySelector(`.cust${v}`).children[1].innerHTML
-    localStorage.setItem("sharenmae",th);    
-    
-
+function addpeople(p){
+    let div = document.createElement('div')
+    div.setAttribute('class',`cust cust${p}`)
+    for(i=0; i<3; i++){
+        let span = document.createElement('span')
+        if(i=0){
+            span.setAttribute('class','cusim')
+        }
+        if(i=1){
+            span.setAttribute('class','cusnam')
+        }
+        if(i=2){
+            span.setAttribute('class','money')
+        }
+        div.appendChild(span)
+    }
+    document.querySelector('.customer').append(div)
 }
-// function reniew(){
-//     const money = localStorage.getItem('owntotal');
-//     if(money){
-//         document.querySelector(`.cust${v}`).children[2].innerHTML = money;
-//     }
-    
-// }
 
 
